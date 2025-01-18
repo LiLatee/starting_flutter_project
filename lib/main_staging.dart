099767bc132bc9b877ae53be28e1f88ea23df562
lilatee <marcin.hradowicz@gmail.com>
2025-01-18 10:24:07 +0100

    stash

--- lib/main_staging.dart
+++ lib/main_staging.dart
@@ -8,9 +8,3 @@ void main() {
     firebaseOptions: DefaultFirebaseOptions.currentPlatform,
   );
 }
-
-// echo "FLAVOR: $FLAVOR"
-// echo "BUILT_PRODUCTS_DIR: $BUILT_PRODUCTS_DIR"
-// echo "SRCROOT: $SRCROOT"
-// echo "CONFIGURATION: $CONFIGURATION"
-// cp "${SRCROOT}/flavors/${FLAVOR}/GoogleService-Info.plist" "${SRCROOT}/${PRODUCT_NAME}"
